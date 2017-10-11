e4b0bcd8889d4a7cda7347e71e9462cbcf557c3b
Joseph <josembi@gmail.com>
2017-10-11 23:32:40 +0200

    Chef Signup with TDD

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,4 +4,29 @@ class ChefsController < ApplicationController
 		@chef = Chef.new
 	end
 
+	def create
+		@chef = Chef.new(kiboko_yao)
+		if @chef.save
+				flash[:success] = "Benvenuto #{@chef.chefname} nell'app di ricette"
+				redirect_to chef_path(@chef)
+			else
+				render 'new'
+			end
+	end
+
+	def show
+		
+	end
+
+
+
+
+	private
+
+	def kiboko_yao
+		params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+		
+	end
+
+
 end
