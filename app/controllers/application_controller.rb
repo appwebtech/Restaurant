d5631c4617970a270d6ee5174e3b2a4d0bd64120
Joseph <josembi@gmail.com>
2017-10-15 09:39:18 +0200

    Add Authentication Methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_chef, :logged_in?
+  
+  def current_chef
+  	@current_chef ||= Chef.find(session[:chef_id]) if session[:chef_id] # If @currentchef is there, return it right away, if not grab current chef then return it.
+  end
+
+  def logged_in?
+  	!!current_chef    
+  end
+
+  def require_user
+  	if !logged_in?   # if not logged_in
+  		flash[:danger] = "Devi eseguire l'accesso per complettare la richiesta"
+  		redirect_to root_path
+  	end
+  end
 end
