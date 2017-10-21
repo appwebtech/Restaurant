395a5f09e38b3be602d00a06bfd0ce260dd11f3e
Joseph <josembi@gmail.com>
2017-10-22 00:10:32 +0200

    Admin Restrictions on Ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,19 @@
 class IngredientsController < ApplicationController 
 	before_action :viungo, only: [:edit, :update, :show]
+	before_action :require_admin, except: [:show, :index]
 
 	def new
-		
+		@ingredient = Ingredient.new
 	end
 
 	def create
-		
+		@ingredient = Ingredient.new(uvoo_waku)
+		if @ingredient.save
+			flash[:success] = "Ingrediente creato con successo"
+			redirect_to ingredient_path(@ingredient)
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -14,7 +21,12 @@ class IngredientsController < ApplicationController
 	end
 
 	def update
-		
+		if @ingredient.update(uvoo_waku)
+			flash[:success] = "Il nome di ingrediente modificato con successo"
+			redirect_to @ingredient
+		else
+			render 'edit'
+		end
 	end
 
 	def show
@@ -27,8 +39,19 @@ class IngredientsController < ApplicationController
 
 	private 
 
+	def uvoo_waku
+		params.require(:ingredient).permit(:name)
+	end
+
 	def viungo
 		@ingredient = Ingredient.find(params[:id])
+	end
+
+	def require_admin
+		if !logged_in? || (logged_in? and !current_chef.admin?)
+			flash[:danger] = "Solo l'amministratore può complettare la richiesta"
+			redirect_to ingredients_path 
+		end
 	end
 
 end
